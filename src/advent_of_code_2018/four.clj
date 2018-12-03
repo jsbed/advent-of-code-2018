cf7f8720ce51cdbb423f28a025377d34895e5443
Jean-Samuel B <bedardjs97@hotmail.com>
2018-12-03 01:41:30 -0500

    getting ready for 4th challenge

---
+++ src/advent_of_code_2018/four.clj
@@ -0,0 +1,8 @@
+(ns advent-of-code-2018.four
+  (:require [advent-of-code-2018.utils :as utils]))
+
+(defn go-1 []
+  (->> (utils/load-file-lines "resources/4.txt" #"\r\n")))
+
+(defn go-2 []
+  (->> (utils/load-file-lines "resources/4.txt" #"\r\n")))
